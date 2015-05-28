bcd5dbf2fd8ab4dafc19807d14d361f146a362eb
unknown <racchak@gmail.com>
2015-05-27 21:49:04 -0700

    babel and hot-loader with webpack

---
+++ js/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let Login = React.createClass({ 
+
+  render() {
+      return(
+	      <div>
+	      Welcome to login
+	  </div>
+      );
+  }
+});
+
+export default Login;  
